50dedadfdda8886983fbf338f3b239b21d63d5fa
John Andrew Divina <98078051+LazyAndrew1333@users.noreply.github.com>
2023-01-18 21:30:34 +0800

    Update combined.java

--- IP Test Codes/combined.java
+++ IP Test Codes/combined.java
@@ -20,6 +20,30 @@ public class combined {
 	    else return gcd ( y , x );        // if x < y then go ahead and switch them around.
 	}
 	
+	static void printPermutn(String str, String ans)
+    {
+ 
+        // If string is empty
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+ 
+        for (int i = 0; i < str.length(); i++) {
+ 
+            // ith character of str
+            char ch = str.charAt(i);
+ 
+            // Rest of the string after excluding
+            // the ith character
+            String ros = str.substring(0, i) +
+                        str.substring(i + 1);
+ 
+            // Recursive call
+            printPermutn(ros, ans + ch);
+        }
+    }
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -41,6 +65,8 @@ public class combined {
 				
 				System.out.println(result);
 				
+		        printPermutn(input, "");
+				
 			} else if (choice.equalsIgnoreCase("euclid")) {
 				
 				choiceCorrect = true;
@@ -56,6 +82,8 @@ public class combined {
 				
 				System.out.println(result);
 				
+			} else {
+				System.out.println("SA DALAWA KA LANG PUMILI!!!");
 			}
 		}
 	}
